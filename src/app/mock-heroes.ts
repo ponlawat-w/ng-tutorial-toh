1a49e83eafa7f1eb23d5898c873e8e4b91bc8b27
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-06-09 13:29:19 +0700

    Initial Commit - Tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: '優さん'},
+    {id: 12, name: 'なる子'},
+    {id: 13, name: 'ボムバスト'},
+    {id: 14, name: 'セレリタス'},
+    {id: 15, name: 'マグネータ'},
+    {id: 16, name: '消マン'},
+    {id: 17, name: 'ディナマ'},
+    {id: 18, name: 'ＩＱ博士'},
+    {id: 19, name: 'マグマ'},
+    {id: 20, name: 'たいふう'}
+];
