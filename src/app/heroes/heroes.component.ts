53b4efa2e2b65d8c1ab0ef3c8027398c41df471d
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-06-09 13:55:16 +0700

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from './../hero.service';
+import { MessageService } from './../message.service';
+
 import { Hero } from './../hero';
-import { HEROES } from './../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +11,24 @@ import { HEROES } from './../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add('ヒーローサービス：「' + hero.name + '」を選択。');
     this.selectedHero = hero;
   }
 
